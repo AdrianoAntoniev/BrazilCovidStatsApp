08a36ff55873ece9cf174124835b60202dc46705
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-05-10 18:09:15 -0300

    Initial commit

---
+++ CovidAPI/CovidAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidAPIApp.swift
+//  CovidAPI
+//
+//  Created by Adriano Rodrigues Vieira on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
